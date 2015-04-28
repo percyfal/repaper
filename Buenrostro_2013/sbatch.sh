49efe59bb35e51aae6c919469251f98c9b52ca83
Per Unneberg <per.unneberg@scilifelab.se>
2015-04-28 17:11:11 +0200

    * Buenrostro_2013/sbatch.sh: rerun incomplete * Buenrostro_2013/smlconf.yaml: add peakcallers * Treutlein_2014/Snakefile: move data directory * Treutlein_2014/smlconf.yaml: comment out fastq dump

--- Buenrostro_2013/sbatch.sh
+++ Buenrostro_2013/sbatch.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 #SBATCH -p node
 #SBATCH -N 1
-#SBATCH -t 24:00:00
+#SBATCH -n 16
+#SBATCH -t 48:00:00
 #SBATCH -J Buenrostro
 
-snakemake -j 16 atacseq_finaldup
+snakemake -j 16 --rerun-incomplete  atacseq_all
 
